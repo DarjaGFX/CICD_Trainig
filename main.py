649229f45cb6a31c51756acc7f42e19c1579749d
Ali Jafari <Ali.Jafari20@gmail.com>
2023-10-07 15:37:09 +0330

    sss

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'status': 200, 'data': "Hello, world! update number 2"}
+    return {'status': 200, 'data': "Hello, world! update number 3"}
 
 
 @app.get("/file")
